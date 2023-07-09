3eb3ca5881d7d3aaa2bf59428694c3f62eaa13c0
Adam <user@Users-MacBook-Pro.local>
2023-07-09 16:16:39 -0600

    experiences and servies stylied and up

--- src/componets/experience/Experience.jsx
+++ src/componets/experience/Experience.jsx
@@ -1,10 +1,142 @@
 import React from 'react'
 import './experience.css'
-
+import {BsFillPatchCheckFill} from 'react-icons/bs'
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
-  )
+    <section id="experience">
+      <h5>What Skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Developement</h3>
+          <div className="experience__content">
+            {/* this can be anothe componet that repeats */}
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Redux</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Jest</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Jasmine</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        {/* front end list ends here */}
+        <div className="experience__backend">
+          <h3>Backend Developement</h3>
+          <div className="experience__content">
+            {/* this can be anothe componet that repeats */}
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Node JS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>PHP</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Flask</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>JQuery</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>SQL</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>PostgressSQL</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>SQLAlchemy</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Experience
--- src/componets/portfolio/Portfolio.jsx
+++ src/componets/portfolio/Portfolio.jsx
@@ -1,9 +1,68 @@
 import React from 'react'
 import './portfolio.css'
+import StockImg from '../../assests/stock.jpeg'
+import StripeImg from '../../assests/stripe-project.jpeg'
+import TwitterImg from '../../assests/twitter-clone.jpeg'
+
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={StockImg} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">Github</a>
+          <a href="https://.com" className='btn' target='_blank' rel="noreferrer">Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={StripeImg} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">Github</a>
+          <a href="https://.com" className='btn' target='_blank' rel="noreferrer">Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={TwitterImg} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">Github</a>
+          <a href="https://.com" className='btn' target='_blank' rel="noreferrer">Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={StockImg} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">Github</a>
+          <a href="https://.com" className='btn' target='_blank' rel="noreferrer">Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={StockImg} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">Github</a>
+          <a href="https://.com" className='btn' target='_blank' rel="noreferrer">Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={StockImg} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">Github</a>
+          <a href="https://.com" className='btn' target='_blank' rel="noreferrer">Live Demo</a>
+        </article>
+      </div>
+
+    </section>
   )
 }
 
